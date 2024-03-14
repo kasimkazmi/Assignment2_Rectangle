397494e2b4f8db899848a8309e92a353cce349c7
Kasim kazmi <kasimkazmi27@gmail.com>
2024-03-14 15:49:01 -0400

    Minor Changes and Comments added

--- Assignment2_Rectangle/Program.cs
+++ Assignment2_Rectangle/Program.cs
@@ -1,41 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Assignment2;
 
 internal class Program
 {
     public static void Main(string[] args)
     {
-        
+        // Create a new rectangle with default length and width of 1
         var rectangle = new Rectangle();
 
-        
+        // Display information about the default rectangle
         DisplayRectangleInfo("Default Rectangle", rectangle);
 
-        
+        // Prompt the user to enter dimensions for a new rectangle
         Console.WriteLine("\nEnter dimensions for a new rectangle:");
 
-        
+        // Prompt the user to enter the length
         Console.Write("Enter length: ");
         int userLength = Convert.ToInt32(Console.ReadLine());
+
+        // Set the length of the rectangle
         rectangle.SetLength(userLength);
 
-
+        // Prompt the user to enter the width
         Console.Write("Enter width: ");
         int userWidth = Convert.ToInt32(Console.ReadLine());
+
+        // Set the width of the rectangle
         rectangle.SetWidth(userWidth);
 
-
-        
+        // Display information about the user-defined rectangle
         DisplayRectangleInfo("User Rectangle", rectangle);
     }
 
     static void DisplayRectangleInfo(string title, Rectangle rectangle)
     {
+        // Display the title
         Console.WriteLine($"\n{title} - Length: {rectangle.GetLength()}, Width: {rectangle.GetWidth()}");
+
+        // Display the perimeter and area of the rectangle
         Console.WriteLine($"Perimeter: {rectangle.GetPerimeter()}, Area: {rectangle.GetArea()}");
     }
 }
--- Assignment2_Rectangle/Rectangle.cs
+++ Assignment2_Rectangle/Rectangle.cs
@@ -1,15 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Assignment2
 {
+    /// Represents a rectangle with a length and width.
     public class Rectangle
     {
+        /// The length of the rectangle.
+
         private int length;
+
+
+        /// The width of the rectangle.
+
         private int width;
+
+
+        /// Initializes a new instance of the Rectangle class with a default length and width of 1.
 
         public Rectangle()
         {
@@ -17,47 +23,97 @@ namespace Assignment2
             width = 1;
         }
 
+        /// Initializes a new instance of the Rectangle class with the specified length and width.
+
+        /// <param name="length">The length of the rectangle.</param>
+        /// <param name="width">The width of the rectangle.</param>
         public Rectangle(int length, int width)
         {
-            this.length = Math.Max(1, length); 
-            this.width = Math.Max(1, width);  
+            if (length > 0 && width > 0)
+            {
+                this.length = length;
+                this.width = width;
+            }
+            else
+            {
+                throw new ArgumentException("Length and width must be greater than 0.");
+                /// <exception cref="ArgumentException">Thrown when either length or width is less than or equal to 0.</exception>
+
+            }
         }
+
+
+        /// Gets the length of the rectangle.
 
         public int GetLength()
         {
+            /// <returns>The length of the rectangle.</returns>
+
             return length;
         }
 
+
+        /// Sets the length of the rectangle.
+        /// <param name="length">The new length of the rectangle.</param>
         public void SetLength(int length)
         {
             if (length > 0)
             {
                 this.length = length;
             }
-            
+            else
+            {
+                throw new ArgumentException("Length must be greater than 0.");
+                /// <exception cref="ArgumentException">Thrown when length is less than or equal to 0.</exception>
+
+            }
         }
 
+
+        /// Gets the width of the rectangle.
         public int GetWidth()
         {
+            /// <returns>The width of the rectangle.</returns>
+
             return width;
         }
 
+        /// Sets the width of the rectangle.
+
+        /// <param name="width">The new width of the rectangle.</param>
         public void SetWidth(int width)
         {
             if (width > 0)
             {
                 this.width = width;
             }
-            
+            else
+            {
+                throw new ArgumentException("Width must be greater than 0.");
+                /// <exception cref="ArgumentException">Thrown when width is less than or equal to 0.</exception>
+
+            }
         }
+
+
+        /// Gets the perimeter of the rectangle.
 
         public int GetPerimeter()
         {
+            /// <returns>The perimeter of the rectangle.</returns>
+
             return 2 * (length + width);
+
         }
 
+
+        /// Gets the area of the rectangle.
+
         public int GetArea()
+
         {
+            /// <returns>The area of the rectangle.</returns>
+
             return length * width;
         }
     }
